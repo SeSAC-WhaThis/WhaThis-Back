00ef3683f7b55a5a89cbe4afe1b4891db3d8b1b6
sshkim1013 <sshkim1013@naver.com>
2025-12-14 16:18:14 +0900

    예외 처리 클래스 정의

---
+++ src/main/java/com/example/whathis/exception/ApiException.java
@@ -0,0 +1,11 @@
+package com.example.whathis.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+}
