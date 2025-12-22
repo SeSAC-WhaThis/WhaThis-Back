eebec04ce3ecdf76ddc30fa26cd5ca63fd69238d
fhdl1221 <fhdl353212@gmail.com>
2025-12-22 14:13:44 +0900

    refactor/user: 로그인 및 회원가입 응답 형식 변경

--- src/main/java/com/example/whathis/user/controller/UserController.java
+++ src/main/java/com/example/whathis/user/controller/UserController.java
@@ -9,6 +9,7 @@ import com.example.whathis.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,19 +25,24 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<UserResponse>> signup(
+    public ResponseEntity<ApiResponse<Void>> signup(
             @Valid @RequestBody SignupRequest request
     ) {
-        UserResponse response = userService.signup(request);
+        userService.signup(request);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.success(response));
+                .body(ApiResponse.successWithMessage("회원가입이 완료되었습니다."));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<TokenResponse>> login(
+    public ResponseEntity<ApiResponse<Void>> login(
             @Valid @RequestBody LoginRequest request
     ) {
         TokenResponse response = userService.login(request);
-        return ResponseEntity.ok(ApiResponse.success(response));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + response.getAccessToken());
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(ApiResponse.successWithMessage("로그인이 완료되었습니다."));
     }
 }
