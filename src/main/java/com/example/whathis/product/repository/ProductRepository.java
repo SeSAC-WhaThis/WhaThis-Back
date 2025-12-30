c48b97d5d3d9eff9f466588e7f03c53e2dad9dab
sshkim1013 <sshkim1013@naver.com>
2025-12-30 15:46:23 +0900

    제품 전체 조회 시, 카테고리의 가나다 순으로 정렬

--- src/main/java/com/example/whathis/product/repository/ProductRepository.java
+++ src/main/java/com/example/whathis/product/repository/ProductRepository.java
@@ -1,8 +1,6 @@
 package com.example.whathis.product.repository;
 
 import com.example.whathis.product.entity.Product;
-import com.example.whathis.user.entity.User;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
@@ -15,26 +13,27 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // N+1 방지: seller, category를 fetch join으로 한 번에 조회
-    @Query("SELECT p FROM Product p " +
-           "JOIN FETCH p.seller " +
-           "LEFT JOIN FETCH p.category")
-    List<Product> findAllWithSellerAndCategory();
+   // N+1 방지: seller, category를 fetch join으로 한 번에 조회
+   @Query("SELECT p FROM Product p " +
+          "JOIN FETCH p.seller " +
+          "LEFT JOIN FETCH p.category " +
+          "ORDER BY p.category.name ASC")
+   List<Product> findAllWithSellerAndCategory();
     
-    // 페이징 & N+1 방지
-    // 프론트엔드와의 연동을 위해서 오버로딩
+   // 페이징 & N+1 방지
+   // 프론트엔드와의 연동을 위해서 오버로딩
     @Query("SELECT p FROM Product p " +
            "JOIN FETCH p.seller " +
            "LEFT JOIN FETCH p.category")
     Page<Product> findAllWithSellerAndCategory(Pageable pageable);
-    
+
     // 단일 조회 & N+1 방지
     @Query("SELECT p FROM Product p " +
            "JOIN FETCH p.seller " +
            "LEFT JOIN FETCH p.category " +
            "WHERE p.id = :productId")
     Optional<Product> findByIdWithSellerAndCategory(@Param("productId") Long productId);
-    
+
     // 판매자별 상품 조회 & N+1 방지
     // 내가 등록한 상품 목록을 조회
     @Query("SELECT p FROM Product p " +
