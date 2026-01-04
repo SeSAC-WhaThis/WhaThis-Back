b76c085d1acb8d96a9ed608fb098be90ea2f34fd
fhdl1221 <fhdl353212@gmail.com>
2026-01-05 02:39:34 +0900

    feat: 포트원 결제 검증 비즈니스 로직 작성

--- src/main/java/com/example/whathis/order/repository/OrderRepository.java
+++ src/main/java/com/example/whathis/order/repository/OrderRepository.java
@@ -4,7 +4,9 @@ import com.example.whathis.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    Optional<Order> findByOrderNumber(String orderNumber);
 }
