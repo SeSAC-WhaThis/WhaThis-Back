c3d97fa833782649f51ebf286c472780630477bf
fhdl1221 <fhdl353212@gmail.com>
2026-01-06 17:27:35 +0900

    feat: 본인 상품 구매 에러 코드 추가

--- src/main/java/com/example/whathis/common/exception/ErrorCode.java
+++ src/main/java/com/example/whathis/common/exception/ErrorCode.java
@@ -28,6 +28,7 @@ public enum ErrorCode {
     ACCESS_DENIED("ACCESS_DENIED", "접근이 거부되었습니다"),
     CANNOT_SELF_LIKE("CANNOT_SELF_LIKE", "본인의 제품에는 좋아요를 누를 수 없습니다."),
     CANNOT_SELF_FOLLOW("CANNOT_SELF_FOLLOW", "자기 자신은 팔로우할 수 없습니다."),
+    CANNOT_SELF_ORDER("CANNOT_SELF_ORDER", "본인의 상품은 주문할 수 없습니다."),
     
     // 404 Not Found
     NOT_FOUND("NOT_FOUND", "리소스를 찾을 수 없습니다"),
